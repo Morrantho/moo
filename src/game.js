1c4049f05056b7638d20c842aff328fc89b2c553
morrantho <morrantho@gmail.com>
2022-02-08 04:42:55 -0500

    Added Module Include Support. Determines whether a source file is a module if it has a .Load() method. It executes it, awaits, and then resolves once all of its source files resolve.

--- src/game.js
+++ src/game.js
@@ -12,7 +12,7 @@ Game=class
 		let e=new Mu.Phys.Emitter(400,400);
 		e.PushFunction(Mu.Phys.Particle.DynamicFire);
 		e.SetInfinite();
-		e.SetFrequency(0);
+		e.SetFrequency(0); /* <-- this is expensive, dont do it often. */
 	}
 };
 Game.Load();
